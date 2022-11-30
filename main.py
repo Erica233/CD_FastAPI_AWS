808801f7425e26a6d4bb981086a5b5291fb9aa67
Fangting Ma <fm128@duke.edu>
2022-11-30 01:33:50 +0000

    test

--- main.py
+++ main.py
@@ -16,27 +16,9 @@ def add(num1: int, num2: int):
 
     total = num1 + num2
     return {"total": total}
-    
-@app.get("/query/{food}")
-async def query(food: str):
-    """Query calories by food name"""
-
-    url = "https://calorieninjas.p.rapidapi.com/v1/nutrition"
-
-    querystring = {"query":food}
-
-    headers = {
-        "X-RapidAPI-Key": "155edcf844msh7d2f8265bf24668p17f256jsn999e77a74d46",
-        "X-RapidAPI-Host": "calorieninjas.p.rapidapi.com"
-    }
-
-    response = requests.request("GET", url, headers=headers, params=querystring, timeout=10)
-
-    return {"result": response.text}
-
 
 @app.get("/query/{date}")
-async def query2(date: str):
+async def query(date: str):
     """Query covid19 statistics by location and date"""
 
     url = "https://rapidapi.com/axisbits-axisbits-default/api/covid-19-statistics/"
